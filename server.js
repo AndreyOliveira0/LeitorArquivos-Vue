e19382c1e67afad253aecbc4c269d1035865171a
caioh230 <caiohchagas92@gmail.com>
2025-06-10 19:05:57 -0300

    Alterações para a melhor execução do docker

--- server.js
+++ server.js
@@ -2,11 +2,14 @@ const express = require('express');
 const path = require('path');
 const app = express();
 
+// Servindo os arquivos estáticos gerados pelo Vite
 app.use(express.static(path.join(__dirname, 'dist')));
 
+// Serve o index.html para qualquer rota não encontrada
 app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname, 'dist', 'index.html'));
 });
 
+// Porta padrão (3000 ou o valor configurado no ambiente)
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => console.log(`Frontend rodando na porta ${PORT}`));
